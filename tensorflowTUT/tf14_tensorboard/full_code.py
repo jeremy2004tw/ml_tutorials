c32eb249a5314e40a2a508403c4576b54417fd66
Jeremy <jeremy2004tw@gmail.com>
2026-07-18 16:59:08 -0500

    fix tensorflow compatibility issue

--- tensorflowTUT/tf14_tensorboard/full_code.py
+++ tensorflowTUT/tf14_tensorboard/full_code.py
@@ -9,14 +9,17 @@ Please note, this code is only for python 3+. If you are using python 2+, please
 from __future__ import print_function
 import tensorflow as tf
 
+# Enable TensorFlow 1.x-style graph execution for this tutorial example
+tf.compat.v1.disable_eager_execution()
+
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
     with tf.name_scope('layer'):
         with tf.name_scope('weights'):
-            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+            Weights = tf.compat.v1.Variable(tf.compat.v1.random_normal([in_size, out_size]), name='W')
         with tf.name_scope('biases'):
-            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            biases = tf.compat.v1.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
         if activation_function is None:
@@ -28,8 +31,8 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
 
 # define placeholder for inputs to network
 with tf.name_scope('inputs'):
-    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
-    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
+    xs = tf.compat.v1.placeholder(tf.float32, [None, 1], name='x_input')
+    ys = tf.compat.v1.placeholder(tf.float32, [None, 1], name='y_input')
 
 # add hidden layer
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
@@ -38,26 +41,15 @@ prediction = add_layer(l1, 10, 1, activation_function=None)
 
 # the error between prediciton and real data
 with tf.name_scope('loss'):
-    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
-                                        reduction_indices=[1]))
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), axis=1))
 
 with tf.name_scope('train'):
-    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+    train_step = tf.compat.v1.train.GradientDescentOptimizer(0.1).minimize(loss)
 
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 
-# tf.train.SummaryWriter soon be deprecated, use following
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:  # tensorflow version < 0.12
-    writer = tf.train.SummaryWriter('logs/', sess.graph)
-else: # tensorflow version >= 0.12
-    writer = tf.summary.FileWriter("logs/", sess.graph)
-
-# tf.initialize_all_variables() no long valid from
-# 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    init = tf.initialize_all_variables()
-else:
-    init = tf.global_variables_initializer()
+writer = tf.compat.v1.summary.FileWriter("logs/", sess.graph)
+init = tf.compat.v1.global_variables_initializer()
 sess.run(init)
 
 # direct to the local dir and run this in terminal:
