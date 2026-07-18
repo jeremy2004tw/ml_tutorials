82a5e5888ae44f4eb6bd54511ba62247e20062c5
Jeremy <jeremy2004tw@gmail.com>
2026-07-18 16:08:10 -0500

    fix tensorflow compatibility issue

--- tensorflowTUT/tensorflow7_variable.py
+++ tensorflowTUT/tensorflow7_variable.py
@@ -9,21 +9,19 @@ Please note, this code is only for python 3+. If you are using python 2+, please
 from __future__ import print_function
 import tensorflow as tf
 
-state = tf.Variable(0, name='counter')
+# Enable TensorFlow 1.x-style graph execution for this tutorial example
+tf.compat.v1.disable_eager_execution()
+
+state = tf.compat.v1.Variable(0, name='counter')
 #print(state.name)
 one = tf.constant(1)
 
 new_value = tf.add(state, one)
-update = tf.assign(state, new_value)
+update = tf.compat.v1.assign(state, new_value)
 
-# tf.initialize_all_variables() no long valid from
-# 2017-03-02 if using tensorflow >= 0.12
-if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:
-    init = tf.initialize_all_variables()
-else:
-    init = tf.global_variables_initializer()
+init = tf.compat.v1.global_variables_initializer()
 
-with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
     sess.run(init)
     for _ in range(3):
         sess.run(update)
