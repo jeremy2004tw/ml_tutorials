07039fcfb872f100c1dd56ba4c1687eda0be4447
Jeremy <jeremy2004tw@gmail.com>
2026-07-18 16:03:07 -0500

    fix tensorflow compatibility issue

--- tensorflowTUT/tensorflow6_session.py
+++ tensorflowTUT/tensorflow6_session.py
@@ -9,19 +9,22 @@ Please note, this code is only for python 3+. If you are using python 2+, please
 from __future__ import print_function
 import tensorflow as tf
 
+# Enable TensorFlow 1.x-style graph execution for this tutorial example
+tf.compat.v1.disable_eager_execution()
+
 matrix1 = tf.constant([[3, 3]])
 matrix2 = tf.constant([[2],
                        [2]])
 product = tf.matmul(matrix1, matrix2)  # matrix multiply np.dot(m1, m2)
 
 # method 1
-sess = tf.Session()
+sess = tf.compat.v1.Session()
 result = sess.run(product)
 print(result)
 sess.close()
 
 # method 2
-with tf.Session() as sess:
+with tf.compat.v1.Session() as sess:
     result2 = sess.run(product)
     print(result2)
 
